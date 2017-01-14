a007906a6d1c3d860ef6606263547c3d93ac9fd0
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-01-14 14:13:28 -0500

    finished too much

---
+++ Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Alex Bearinger on 2017-01-14.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
---
+++ Vehicles/Motorcycle.h
@@ -0,0 +1,15 @@
+//
+//  Motorcycle.h
+//  Vehicles
+//
+//  Created by Alex Bearinger on 2017-01-14.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Motorcycle : Vehicle
+
+@property (nonatomic, strong) NSString *engineNoise;
+
+@end
